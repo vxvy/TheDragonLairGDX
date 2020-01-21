b853ff37a575ef06c63f586670ef39d0008fc321
vxvy <erioyakigmail.com>
2020-01-21 18:06:52 +0100

    firstScene
    trying to figure out how to do things

--- core/src/com/example/thedragonslair/MyGdxGame.java
+++ core/src/com/example/thedragonslair/MyGdxGame.java
@@ -1,49 +1,24 @@
 package com.example.thedragonslair;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
-import java.awt.Color;
-
-import static javax.swing.UIManager.getColor;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.example.thedragonslair.Scenes.TitleScreen;
 
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 
-	private Stage stage;
-	Texture texture;
-	Image image1;
+	static public Skin gameSkin;
 
 	@Override
 	public void create () {
-		this.stage = new Stage(new ScreenViewport());
-		this.texture = new Texture(Gdx.files.internal("menustuff\\background\\menuprovisionalbackground.png"));
-		this.image1 = new Image(texture);
-		this.image1.setPosition(
-				Gdx.graphics.getWidth()/3-image1.getWidth()/2,
-				Gdx.graphics.getHeight()*2/3-image1.getHeight()/2
-		);
-		this.image1.setPosition(
-				0,
-				Gdx.graphics.getHeight()
-		);
-		this.stage.addActor(image1);
+		gameSkin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+		this.setScreen(new TitleScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1,1,1,1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act();
-		stage.draw();
-
-
+		super.render();
 	}
 	
 	@Override
---
+++ core/src/com/example/thedragonslair/Scenes/TitleScreen.java
@@ -0,0 +1,62 @@
+package com.example.thedragonslair.Scenes;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.example.thedragonslair.MyGdxGame;
+
+public class TitleScreen implements Screen {
+
+    private Stage stage;
+    private Game game;
+
+    public TitleScreen(Game game) {
+        this.game = game;
+        stage = new Stage(new ScreenViewport());
+
+        Label title = new Label("Title Screen", MyGdxGame.gameSkin,"big-black");
+        title.setAlignment(Align.center);
+        title.setY(Gdx.graphics.getHeight()*2/3);
+        title.setWidth(Gdx.graphics.getWidth());
+        stage.addActor(title);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
